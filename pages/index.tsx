d5716684c351fddffe639961a7133dc9b3c47516
Sam Tarakajian <sam@cycling74.com>
2021-06-16 13:30:56 -0400

    finished typescript switch, got rid of the api routes (shouldn't be necessary)

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      Hi I guess this is a page
+    </div>
+  )
+}
