505d8003a6987190a640ec5e7b547d18e7f37ec1
Florian Demmer <florian@cycling74.com>
2021-08-04 16:39:03 +0100

    introduced separate pages for IO and Parameters to test routing setup

---
+++ src/pages/io.tsx
@@ -0,0 +1,10 @@
+import Device from "../components/Device";
+
+export default function IO() {
+	return (
+		<div>
+			<h1>IO</h1>
+			<Device />
+		</div>
+	);
+}
---
+++ src/pages/parameters.tsx
@@ -0,0 +1,10 @@
+import Device from "../components/Device";
+
+export default function Parameters() {
+	return (
+		<div>
+			<h1>Parameters</h1>
+			<Device />
+		</div>
+	);
+}
