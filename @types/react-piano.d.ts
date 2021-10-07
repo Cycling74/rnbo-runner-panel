6312e0a411c149f5c4da7c1cff2637dad24b4257
Florian Demmer <florian@cycling74.com>
2021-10-07 09:54:44 +0100

    added missing module type declarations for react-piano and osc

---
+++ @types/react-piano.d.ts
@@ -0,0 +1,29 @@
+declare module "react-piano" {
+	import { Component, ReactNode } from "react";
+
+	export type KeyboardShortcut = {
+		key: string;
+		midiNumber: number;
+	}
+
+	export class Piano extends Component<{
+		noteRange: { first: number; last: number };
+		playNote: (midi: number) => any;
+		stopNote: (midi: number) => any;
+		width?: number;
+		activeNotes?: number[];
+		keyWidthToHeight?: number;
+		renderNoteLabel?: ({ keyboardShortcut: any, midiNumber: number, isActive: boolean, isAccidental: boolean }) => ReactNode;
+		className?: string;
+		disabled?: boolean;
+		keyboardShortcuts?: KeyboardShortcut[];
+		onPlayNoteInput?: (midi: number, { prevActiveNotes }:  { prevActiveNotes: number[] }) => any;
+		onStopNoteInput?: (midi: number, { prevActiveNotes }:  { prevActiveNotes: number[] }) => any;
+	}>{};
+
+	interface MidiNumbersHelpers {
+		fromNote: (note: string) => number;
+	}
+
+	export const MidiNumbers: MidiNumbersHelpers;
+}
