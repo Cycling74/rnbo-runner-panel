e2fee84acfe7a08491c11e3e65d338260f7276ea
Alex Norman <xnor@cycling74.com>
2021-10-12 09:54:26 -0700

    bugfix packaging script with new capitalized folder name

--- scripts/package-debian.mjs
+++ scripts/package-debian.mjs
@@ -10,8 +10,8 @@ const debian = path.join(path.resolve(basedir, "debian"));
 const version = JSON.parse(fs.readFileSync(path.join(basedir, 'package.json')))["version"];
 
 //add the version into the control file
-const control = fs.readFileSync(path.join(debian, "debian", "control.in"), 'utf8').replace(/[\s\n]*$/, '') + `\nVersion: ${version}\n`;
-fs.writeFileSync(path.join(debian, "debian", "control"), control);
+const control = fs.readFileSync(path.join(debian, "DEBIAN", "control.in"), 'utf8').replace(/[\s\n]*$/, '') + `\nVersion: ${version}\n`;
+fs.writeFileSync(path.join(debian, "DEBIAN", "control"), control);
 
 //cleanup if we have an existing export
 fs.rmSync(path.join(debian, "usr"), { recursive: true, force: true });
