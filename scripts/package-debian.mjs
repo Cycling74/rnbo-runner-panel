ebd0545702fbf0c8e068ad12e7307c5777cf9ec2
Florian Demmer <florian@cycling74.com>
2021-10-12 19:13:35 +0100

    package-debian util: minor adjustment plus consider PKG_NAME and PKG_VERSION with sensible defaults

--- scripts/package-debian.mjs
+++ scripts/package-debian.mjs
@@ -1,21 +1,27 @@
-import path from "path";
-import fs from "fs-extra";
-import { fileURLToPath } from 'url';
+import { dirname, join, resolve } from "path";
+import { readFileSync, writeFileSync, rmSync, copySync } from "fs-extra";
+import { fileURLToPath } from "url";
 import { execSync } from "child_process";
 
-const name = "rnbo-runner-panel";
+const basedir = resolve(dirname(fileURLToPath(import.meta.url)), "..");
+const debian = join(resolve(basedir, "debian"));
 
-const basedir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "..");
-const debian = path.join(path.resolve(basedir, "debian"));
-const version = JSON.parse(fs.readFileSync(path.join(basedir, 'package.json')))["version"];
+const readPkgInfoVersion = fpath => {
+	const info = JSON.parse(readFileSync(fpath, { encoding: "utf8"} ));
+	if (!info.version) throw new Error("Missing version property in pacakge.json file");
+	return info.version;
+};
 
-//add the version into the control file
-const control = fs.readFileSync(path.join(debian, "DEBIAN", "control.in"), 'utf8').replace(/[\s\n]*$/, '') + `\nVersion: ${version}\n`;
-fs.writeFileSync(path.join(debian, "DEBIAN", "control"), control);
+const name = process.env.PKG_NAME || "rnbo-runner-panel";
+const version = process.env.PKG_VERSION || readPkgInfoVersion(join(basedir, "package.json"));
 
-//cleanup if we have an existing export
-fs.rmSync(path.join(debian, "usr"), { recursive: true, force: true });
-fs.copySync(path.join(basedir, "out"), path.join(debian, "usr", "share", name, "www"), { overwrite: true } );
+// add the version into the control file
+const control = readFileSync(join(debian, "DEBIAN", "control.in"), "utf8").replace(/[\s\n]*$/, "") + `\nVersion: ${version}\n`;
+writeFileSync(join(debian, "DEBIAN", "control"), control);
+
+// cleanup if we have an existing export
+rmSync(join(debian, "usr"), { recursive: true, force: true });
+copySync(join(basedir, "out"), join(debian, "usr", "share", name, "www"), { overwrite: true } );
 
 const deb = `${name}_${version}.deb`;
 
