3ad13e4da642969bcdcc639b1f32cb290856ae44
Florian Demmer <florian@cycling74.com>
2024-02-20 19:09:02 +0000

    slightly better actions in Settings Menu

--- src/components/settings/list.tsx
+++ src/components/settings/list.tsx
@@ -1,17 +1,21 @@
 import { Stack } from "@mantine/core";
-import { BaseSettingsItemProps, SettingsItem } from "./item";
+import { BaseSettingsItemProps, SettingActionProps, SettingsAction, SettingsItem } from "./item";
 import { FunctionComponent, memo } from "react";
 
 export type SettingsListProps = {
+	actions?: Array<SettingActionProps>;
 	items: Array<BaseSettingsItemProps>;
 };
 
-const SettingsList: FunctionComponent<SettingsListProps> = memo(function SettingsListWrapper({ items }: SettingsListProps) {
+const SettingsList: FunctionComponent<SettingsListProps> = memo(function SettingsListWrapper({ actions, items }: SettingsListProps) {
 
 	return (
 		<Stack gap="xs">
 			{
 				items.map(item => <SettingsItem key={ item.name } { ...item } />)
+			}
+			{
+				actions?.map(action => <SettingsAction key={ action.label } { ...action } /> )
 			}
 		</Stack>
 	);
