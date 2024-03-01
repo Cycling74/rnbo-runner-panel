8d0a85a40bb4b3afdb9b797bb3014b1e8444fa29
Florian Demmer <florian@cycling74.com>
2024-03-01 14:18:32 +0000

    parametermodel: removed commented out code block

--- src/models/parameter.ts
+++ src/models/parameter.ts
@@ -45,31 +45,6 @@ export class ParameterRecord extends ImmuRecord<ParameterRecordProps>({
 		return result;
 	}
 
-	// public static arrayFromDescription(desc: JsonMap, prefix?: string): ParameterRecord[] {
-
-	// 	if (typeof desc.VALUE !== "undefined") {
-
-	// 		const parameterRecord = new ParameterRecord({
-	// 			name: prefix,
-	// 			value: (desc.VALUE as number),
-	// 			min: ((desc.RANGE as JsonMap[])[0].MIN as number),
-	// 			max: ((desc.RANGE as JsonMap[])[0].MAX as number),
-	// 			type: (desc.TYPE as string),
-	// 			normalizedValue: (((desc.CONTENTS as JsonMap).normalized as JsonMap).VALUE as number)
-	// 		});
-
-	// 		return [parameterRecord];
-	// 	}
-
-	// 	const nextDesc = desc.CONTENTS as JsonMap;
-	// 	const subparamNames = Object.getOwnPropertyNames(nextDesc);
-	// 	const subparamLists = subparamNames.map(subparamName => {
-	// 		const nextPrefix = prefix ? `${prefix}/${subparamName}` : subparamName;
-	// 		return this.arrayFromDescription(nextDesc[subparamName] as JsonMap, nextPrefix);
-	// 	});
-	// 	return subparamLists.reduce((acc, l) => acc.concat(l), []);
-	// }
-
 	public get id(): string {
 		return this.name;
 	}
