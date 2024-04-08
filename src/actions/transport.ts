32b9e96dbe8d92f8843921411784f200a6421678
Florian Demmer <florian@cycling74.com>
2024-04-08 20:24:34 +0100

    clamp bpm values using BPMRange

--- src/actions/transport.ts
+++ src/actions/transport.ts
@@ -4,6 +4,7 @@ import { ActionBase, AppThunk } from "../lib/store";
 import { OSCQueryRNBOJackTransport, OSCQueryValueType } from "../lib/types";
 import { getShowTransportControl, getTransportControlState } from "../selectors/transport";
 import { clamp } from "../lib/util";
+import { BPMRange } from "../lib/constants";
 
 export enum TransportActionType {
 	INIT = "INIT_TRANSPORT",
@@ -113,7 +114,7 @@ export const setTransportBPMOnRemote = (bpm: number): AppThunk =>
 		oscQueryBridge.sendPacket(writePacket({
 			address: `${oscTransportPathPrefix}/bpm`,
 			args: [{
-				value: bpm,
+				value: clamp(bpm, BPMRange.Min, BPMRange.Max),
 				type: OSCQueryValueType.Float32
 			}]
 		}));
@@ -122,14 +123,14 @@ export const setTransportBPMOnRemote = (bpm: number): AppThunk =>
 export const incrementTransportBPMOnRemote = (scale: number = 1): AppThunk =>
 	(dispatch, getState) => {
 		const state = getState();
-		const bpm = clamp(getTransportControlState(state).bpm + (1 * scale), 1, 2000);
+		const bpm = clamp(getTransportControlState(state).bpm + (1 * scale), BPMRange.Min, BPMRange.Max);
 		dispatch(setTransportBPMOnRemote(bpm));
 	};
 
 export const decrementTransportBPMOnRemote = (scale: number = 1): AppThunk =>
 	(dispatch, getState) => {
 		const state = getState();
-		const bpm = clamp(getTransportControlState(state).bpm - (1 * scale), 1, 2000);
+		const bpm = clamp(getTransportControlState(state).bpm - (1 * scale), BPMRange.Min, BPMRange.Max);
 		dispatch(setTransportBPMOnRemote(bpm));
 	};
 
