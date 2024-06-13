e8353e433f80a974d7abf364b75323e1164352b2
Alex Norman <xnor@cycling74.com>
2024-06-12 17:29:41 -0700

    store latest preset in state

--- src/reducers/sets.ts
+++ src/reducers/sets.ts
@@ -7,13 +7,15 @@ export type SetState = {
 	show: boolean;
 	sets: ImmuMap<GraphSetRecord["id"], GraphSetRecord>;
 	presets: ImmuMap<PresetRecord["id"], PresetRecord>;
+	presetLatest: string;
 };
 
 export const sets = (state: SetState = {
 
 	show: false,
 	sets: ImmuMap<GraphSetRecord["id"], GraphSetRecord>(),
-	presets: ImmuMap<GraphSetRecord["id"], PresetRecord>()
+	presets: ImmuMap<GraphSetRecord["id"], PresetRecord>(),
+	presetLatest: ""
 }, action: GraphSetAction): SetState => {
 
 	switch (action.type) {
@@ -40,7 +42,7 @@ export const sets = (state: SetState = {
 
 			return {
 				...state,
-				presets: ImmuMap<PresetRecord["id"], PresetRecord>(presets.map(p => [p.id, p]))
+				presets: ImmuMap<PresetRecord["id"], PresetRecord>(presets.map(p => [p.id, p.setLatest(p.name === state.presetLatest)]))
 			};
 		}
 
@@ -48,6 +50,7 @@ export const sets = (state: SetState = {
 			const { name } = action.payload;
 			return {
 				...state,
+				presetLatest: name,
 				presets: state.presets.map(preset => { return preset.setLatest(preset.name === name); })
 			};
 		}
