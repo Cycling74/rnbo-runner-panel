51f3e2db9964477553b6678ed3166873bdb3ed38
Alex Norman <xnor@cycling74.com>
2024-06-14 14:07:30 -0700

    remove unneeded func

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -36,12 +36,3 @@ export const formatFileSize = (size: number): string => {
 	const exp = Math.floor(Math.log(size) / Math.log(1000));
 	return (size / Math.pow(1000, exp)).toFixed(exp >= 2 ? 2 : 0) + " " + fileSizeUnits[exp];
 };
-
-export const readFileAsBase64 = (file: File): Promise<string> => {
-	return new Promise<string>((resolve, reject) => {
-		const reader = new FileReader();
-		reader.onload = () => resolve(reader.result as string);
-		reader.onerror = reject;
-		reader.readAsDataURL(file);
-	});
-};
