865728dab3daf54c9fbb5e103d65acbe2b46fdd3
Alex Norman <xnor@cycling74.com>
2024-06-18 16:01:58 -0700

    add components for renaming/destroying patchers

--- src/components/patchers/index.tsx
+++ src/components/patchers/index.tsx
@@ -1,25 +1,45 @@
-import { Alert, Drawer, Group, Stack } from "@mantine/core";
-import { FunctionComponent, memo } from "react";
+import { Alert, Drawer, Group, Stack, Text } from "@mantine/core";
+import { FunctionComponent, memo, useCallback } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVectorSquare } from "@fortawesome/free-solid-svg-icons";
 import { DrawerSectionTitle } from "../page/drawer";
 import { Seq } from "immutable";
 import { PatcherRecord } from "../../models/patcher";
 import { PatcherItem } from "./item";
+import { modals } from "@mantine/modals";
 
 export type PatcherDrawerProps = {
 	open: boolean;
 	onClose: () => any;
 	onLoadPatcher: (patcher: PatcherRecord) => any;
+	onDeletePatcher: (patcher: PatcherRecord) => any;
+	onRenamePatcher: (patcher: PatcherRecord, newName: string) => any;
 	patchers: Seq.Indexed<PatcherRecord>;
 };
 
-const PatcherDrawer: FunctionComponent<PatcherDrawerProps> = memo(function WrappedPresetDrawer({
+const PatcherDrawer: FunctionComponent<PatcherDrawerProps> = memo(function WrappedPatcherDrawer({
 	open,
 	onClose,
 	onLoadPatcher,
+	onDeletePatcher,
+	onRenamePatcher,
 	patchers
 }: PatcherDrawerProps) {
+
+	const onTriggerDeletePatcher = useCallback((p: PatcherRecord) => {
+		modals.openConfirmModal({
+			title: "Delete Patcher",
+			centered: true,
+			children: (
+				<Text size="sm">
+					Are you sure you want to delete the patcher named { `"${p.name}"` }?
+				</Text>
+			),
+			labels: { confirm: "Delete", cancel: "Cancel" },
+			confirmProps: { color: "red" },
+			onConfirm: () => onDeletePatcher(p)
+		});
+	}, [onDeletePatcher]);
 
 	return (
 		<Drawer
@@ -35,7 +55,10 @@ const PatcherDrawer: FunctionComponent<PatcherDrawerProps> = memo(function Wrapp
 						<PatcherItem
 							key={ patcher.id }
 							patcher={ patcher }
-							onLoad={ onLoadPatcher } />
+							onLoad={ onLoadPatcher }
+							onDelete={ onTriggerDeletePatcher }
+							onRename={ onRenamePatcher }
+						/>
 					)) : (
 						<Alert title="No Patch available" variant="light" color="yellow">
 							Please export a RNBO patch to load on the runner.
