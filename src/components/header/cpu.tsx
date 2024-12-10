ef57dd60e15456ec41c9557b632d7a6456f3e010
Florian Demmer <florian@cycling74.com>
2024-12-10 13:56:50 +0000

    cpumeter: fixed forwardref error log

--- src/components/header/cpu.tsx
+++ src/components/header/cpu.tsx
@@ -1,19 +1,19 @@
-import { FunctionComponent, memo } from "react";
+import { forwardRef, FunctionComponent, memo } from "react";
 import classes from "./cpu.module.css";
 
 export type CPUStatusProps = {
 	load: number;
 };
 
-export const CPUStatus: FunctionComponent<CPUStatusProps> = memo(function WrappedCPUStatusComponent({
-	load
-}) {
-	return (
-		<div className={ classes.cpuRoot } >
-			<div className={ classes.cpuBar } style={{ width: `${load}%`}} />
-			<label className={ classes.cpuLabel } >
-				{ `${Math.round(load)}%` }
-			</label>
-		</div>
-	);
-});
+export const CPUStatus: FunctionComponent<CPUStatusProps> = memo(forwardRef<HTMLDivElement, CPUStatusProps>(
+	function WrappedCPUStatusComponent({ load }, ref) {
+		return (
+			<div className={ classes.cpuRoot } ref={ ref } >
+				<div className={ classes.cpuBar } style={{ width: `${load}%`}} />
+				<label className={ classes.cpuLabel } >
+					{ `${Math.round(load)}%` }
+				</label>
+			</div>
+		);
+	}
+));
