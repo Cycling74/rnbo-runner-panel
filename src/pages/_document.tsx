50eb95af7089bd97e2afbffcf9726db85292367d
Florian Demmer <florian@cycling74.com>
2024-12-16 13:56:14 +0000

    remove google font link tag given that we are using Next's font optimization at this point anyway

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
 					<ColorSchemeScript defaultColorScheme="auto" />
 					<style>{`
 						body {
