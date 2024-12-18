905353d027b0b542857aad99f4c2e49bb6d9f460
Florian Demmer <florian@cycling74.com>
2024-12-18 13:58:54 +0000

    added GraphSetView UI shell
    no paraameter selection, rendering, interaction yet but provides shell for creating and editing of views

---
+++ src/components/setViews/item.tsx
@@ -0,0 +1,130 @@
+import { ChangeEvent, KeyboardEvent, MouseEvent, FormEvent, memo, useCallback, useState, useRef, useEffect, FC } from "react";
+import { ActionIcon, Button, Group, Indicator, Menu, TextInput, Tooltip } from "@mantine/core";
+import classes from "./setviews.module.css";
+import { keyEventIsValidForName, replaceInvalidNameChars } from "../../lib/util";
+import { IconElement } from "../elements/icon";
+import { mdiCheck, mdiClose, mdiDotsVertical, mdiPencil, mdiStar, mdiTrashCan } from "@mdi/js";
+import { GraphSetViewRecord } from "../../models/set";
+
+export type GraphSetViewItemProps = {
+	isActive: boolean;
+	onDelete: (set: GraphSetViewRecord) => any;
+	onLoad: (set: GraphSetViewRecord) => any;
+	onRename: (set: GraphSetViewRecord, name: string) => any;
+	setView: GraphSetViewRecord;
+};
+
+export const GraphSetViewItem: FC<GraphSetViewItemProps> = memo(function WrappedGraphSetViewItem({
+	isActive,
+	onDelete,
+	onLoad,
+	onRename,
+	setView
+}: GraphSetViewItemProps) {
+
+	const [isEditing, setIsEditing] = useState<boolean>(false);
+	const [error, setError] = useState<string | undefined>(undefined);
+	const [name, setName] = useState<string>(setView.name);
+	const inputRef = useRef<HTMLInputElement>();
+
+	const toggleEditing = useCallback(() => {
+		if (isEditing) { // reset name upon blur
+			setName(setView.name);
+		}
+		setIsEditing(!isEditing);
+	}, [setIsEditing, isEditing, setView, setName]);
+
+	const onLoadSetView = useCallback((_e: MouseEvent<HTMLButtonElement>) => {
+		onLoad(setView);
+	}, [onLoad, setView]);
+
+	const onRenameSetView = useCallback((e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (!name?.length) {
+			setError("Please provide a valid SetView name");
+		} else {
+			onRename(setView, name);
+		}
+	}, [name, onRename, setView, setError]);
+
+	const onDeleteSetView = useCallback((_e: MouseEvent<HTMLButtonElement>) => {
+		onDelete(setView);
+	}, [onDelete, setView]);
+
+	const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+		setName(replaceInvalidNameChars(e.target.value));
+		if (error && e.target.value?.length) setError(undefined);
+	}, [setName, error, setError]);
+
+	const onKeyDown = useCallback((e: KeyboardEvent<HTMLInputElement>): void => {
+		if (e.key === "Escape") {
+			e.preventDefault();
+			e.stopPropagation();
+			return void toggleEditing();
+		}
+
+		if (!keyEventIsValidForName(e)) {
+			e.preventDefault();
+		}
+	}, [toggleEditing]);
+
+	useEffect(() => {
+		setName(setView.name);
+		setIsEditing(false);
+	}, [setView, setName, setIsEditing]);
+
+	return isEditing ? (
+		<form onSubmit={ onRenameSetView } >
+			<Group align="flex-start">
+				<TextInput
+					className={ classes.setViewNameInput }
+					onChange={ onChange }
+					onKeyDown={ onKeyDown }
+					ref={ inputRef }
+					size="sm"
+					value={ name }
+					error={ error }
+					variant="default"
+				/>
+				<ActionIcon.Group>
+					<ActionIcon variant="subtle" size="md" color="gray" onClick={ toggleEditing } >
+						<IconElement path={ mdiClose } />
+					</ActionIcon>
+					<ActionIcon variant="subtle" size="md" type="submit">
+						<IconElement path={ mdiCheck } />
+					</ActionIcon>
+				</ActionIcon.Group>
+			</Group>
+		</form>
+	) : (
+		<Group gap="xs">
+			<Button
+				fullWidth
+				className={ classes.setViewButton }
+				justify="flex-start"
+				size="sm"
+				leftSection={ isActive ? (
+					<Tooltip label="This view is currently active" >
+						<IconElement path={ mdiCheck } size="xs" color="green" />
+					</Tooltip>
+				) : null }
+				variant="default"
+				onClick={ onLoadSetView }
+			>
+				{ setView.name }
+			</Button>
+			<Menu position="bottom-end" >
+				<Menu.Target>
+					<ActionIcon variant="subtle" color="gray">
+						<IconElement path={ mdiDotsVertical } />
+					</ActionIcon>
+				</Menu.Target>
+				<Menu.Dropdown>
+					<Menu.Label>SetView Actions</Menu.Label>
+					<Menu.Item leftSection={ <IconElement path={ mdiPencil } /> } onClick={ toggleEditing } >Rename</Menu.Item>
+					<Menu.Item color="red" leftSection={ <IconElement path={ mdiTrashCan } /> } onClick={ onDeleteSetView } >Delete</Menu.Item>
+				</Menu.Dropdown>
+			</Menu>
+		</Group>
+	);
+});
---
+++ src/pages/setviews.tsx
@@ -0,0 +1,87 @@
+import { useAppDispatch, useAppSelector } from "../hooks/useAppDispatch";
+import { RootStateType } from "../lib/store";
+import { getGraphSetViewsBySortOrder, getSelectedGraphSetView } from "../selectors/sets";
+import { useCallback } from "react";
+import { ResponsiveButton } from "../components/elements/responsiveButton";
+import { Group, Stack } from "@mantine/core";
+import { mdiKnob } from "@mdi/js";
+import { useDisclosure } from "@mantine/hooks";
+import { createSetViewOnRemote, destroySetViewOnRemote, loadSetView, renameSetViewOnRemote } from "../actions/sets";
+import SetViewDrawer from "../components/setViews";
+import { GraphSetViewRecord } from "../models/set";
+import { getPatcherInstanceParametersBySetView } from "../selectors/patchers";
+
+export default function SetViews() {
+
+	const [setViewDrawerOpen, { open: openSetViewDrawer, close: closeSetViewDrawer }] = useDisclosure();
+	const dispatch = useAppDispatch();
+
+	const [
+		currentSetView,
+		currentSetViewParameters,
+		setViews
+	] = useAppSelector((state: RootStateType) => {
+		const current = getSelectedGraphSetView(state);
+		return [
+			current,
+			current ? getPatcherInstanceParametersBySetView(state, current) : undefined,
+			getGraphSetViewsBySortOrder(state)
+		];
+	});
+
+	const onCreateSetView = useCallback((name: string) => {
+		dispatch(createSetViewOnRemote(name));
+	}, [dispatch]);
+
+	const onLoadSetView = useCallback((view: GraphSetViewRecord) => {
+		dispatch(loadSetView(view));
+	}, [dispatch]);
+
+	const onDeleteSetView = useCallback((view: GraphSetViewRecord) => {
+		dispatch(destroySetViewOnRemote(view))
+	}, [dispatch]);
+
+	const onRenameSetView = useCallback((view: GraphSetViewRecord, name: string) => {
+		dispatch(renameSetViewOnRemote(view, name));
+	}, [dispatch]);
+
+	return (
+		<>
+			<Stack>
+				<Group justify="space-between" wrap="nowrap">
+					<div style={{ flex: "1 2 50%" }} >
+					</div>
+					<Group style={{ flex: "0" }} wrap="nowrap" gap="xs" >
+						<ResponsiveButton
+							label="SetViews"
+							tooltip="Open SetView Menu"
+							icon={ mdiKnob }
+							onClick={ openSetViewDrawer }
+						/>
+					</Group>
+				</Group>
+				<div>
+					{
+						currentSetView && currentSetViewParameters ? (
+							<>
+								{ currentSetView.name }
+								<br/>
+								{ currentSetViewParameters.size } parameters
+							</>
+						) : "No SetView loaded."
+					}
+				</div>
+			</Stack>
+			<SetViewDrawer
+				open={ setViewDrawerOpen }
+				onClose={ closeSetViewDrawer }
+				onCreateSetView={ onCreateSetView }
+				onDeleteSetView={ onDeleteSetView }
+				onLoadSetView={ onLoadSetView }
+				onRenameSetView={ onRenameSetView }
+				currentSetView={ currentSetView }
+				setViews={ setViews }
+			/>
+		</>
+	);
+}
