a756cef804c0d9347e19dd6f89cad969ac90591f
Florian Demmer <florian@cycling74.com>
2025-04-15 15:43:08 +0100

    fixed leftover "Set View" typo

--- src/components/parameter/withSetViewActions.tsx
+++ src/components/parameter/withSetViewActions.tsx
@@ -50,7 +50,7 @@ export function withParameterSetViewActions(
 					{ type: ParameterMenuEntryType.Divider },
 					{ type: ParameterMenuEntryType.Action, icon: mdiArrowUp, action: onTriggerMoveUp, disabled: index === 0, label: "Move Up" },
 					{ type: ParameterMenuEntryType.Action, icon: mdiArrowDown, action: onTriggerMoveDown, disabled: index === listSize - 1, label: "Move Down" },
-					{ type: ParameterMenuEntryType.Action, icon: mdiMinusBox, action: onTriggerRemoveFromSetView, label: "Remove from SetView" }
+					{ type: ParameterMenuEntryType.Action, icon: mdiMinusBox, action: onTriggerRemoveFromSetView, label: "Remove from Parameter View" }
 				]}
 			/>
 		);
