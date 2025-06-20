512c79c4da84a8a04f329ab301d19ab1826a9cb1
Florian Demmer <florian@cycling74.com>
2025-06-20 16:09:05 +0100

    keep using "out" instead of "dist" folder

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			output: {
-				dir: normalize(join(import.meta.dirname, "dist"))
+				dir: normalize(join(import.meta.dirname, "out"))
 			}
 		}
 	},
