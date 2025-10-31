91eecc71f0d336135a49cc76dbd359f7f989b119
Alex Norman <xnor@cycling74.com>
2025-10-31 11:33:08 -0700

    reorg into separate files

---
+++ server/src/config.rs
@@ -0,0 +1,107 @@
+use {
+    crate::filelist::{FileList, FileListItem},
+    clap::Parser,
+    rocket::{
+        State, delete,
+        fs::{FileServer, NamedFile, TempFile},
+        get, main, put,
+        response::status::NoContent,
+        routes,
+        serde::{Serialize, json::Json},
+        uri,
+    },
+    rocket_dyn_templates::{Template, context},
+    serde::Deserialize,
+    std::{
+        collections::{HashMap, HashSet},
+        fs::File,
+        io::BufReader,
+        path::{Path, PathBuf},
+    },
+};
+
+pub struct Config {
+    pub filetype_paths: HashMap<String, PathBuf>,
+    pub deleteable_filetypes: HashSet<String>,
+}
+
+#[derive(Deserialize, Default)]
+pub struct RunnerConfig {
+    pub backup_dir: Option<PathBuf>,
+    pub datafile_dir: Option<PathBuf>,
+    pub compile_cache_dir: Option<PathBuf>,
+    pub package_dir: Option<PathBuf>,
+    pub source_cache_dir: Option<PathBuf>,
+
+    pub export_dir: Option<PathBuf>,
+    pub save_dir: Option<PathBuf>,
+
+    //file path
+    pub db_path: Option<PathBuf>,
+}
+
+impl RunnerConfig {
+    pub fn read_or_default(config_path: &PathBuf) -> Self {
+        if std::path::Path::exists(&config_path) {
+            if let Ok(file) = File::open(&config_path) {
+                let reader = BufReader::new(file);
+                serde_json::from_reader(reader).unwrap_or_default()
+            } else {
+                Self::default()
+            }
+        } else {
+            Self::default()
+        }
+    }
+}
+
+impl Config {
+    pub fn filetypelist(&self) -> FileList {
+        let mut items = Vec::new();
+        for filetype in self.filetype_paths.keys() {
+            items.push(FileListItem {
+                name: filetype.clone(),
+                uri: uri!("/files", crate::routes::list_json(filetype)).to_string(),
+            });
+        }
+        FileList::new_sorted("filetypes", items)
+    }
+
+    pub fn filetype_path(&self, filetype: &str) -> Option<&PathBuf> {
+        self.filetype_paths.get(filetype)
+    }
+
+    pub fn deleteable_filetype_path(&self, filetype: &str) -> Option<&PathBuf> {
+        if self.deleteable_filetypes.contains(filetype) {
+            self.filetype_paths.get(filetype)
+        } else {
+            None
+        }
+    }
+
+    pub fn filelist(&self, filetype: &str) -> Option<FileList> {
+        self.filetype_path(filetype).map(|path| {
+            let mut items = Vec::new();
+            if let Ok(dir) = std::fs::read_dir(path) {
+                for entry in dir {
+                    if let Ok(entry) = entry {
+                        let path = entry.path();
+                        if !path.is_dir()
+                            && let Some(name) = path.file_name()
+                            && let Some(name) = name.to_str()
+                            && !name.starts_with(".")
+                        {
+                            let item = FileListItem {
+                                name: name.to_owned(),
+                                uri: uri!("/files", crate::routes::download(filetype, name))
+                                    .to_string(),
+                            };
+                            items.push(item);
+                        }
+                    }
+                }
+            }
+            FileList::new_sorted(filetype, items)
+        })
+    }
+}
