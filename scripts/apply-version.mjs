9038e0560d6d214c31bf8dc61a5e4afa73ca7a5b
Alex Norman <xnor@cycling74.com>
2025-11-04 17:06:20 -0800

    increment rust project version after setting package version

---
+++ scripts/apply-version.mjs
@@ -0,0 +1,9 @@
+import { execSync } from "child_process";
+import { readPkgInfoVersion } from "./utils.mjs";
+import { dirname, join, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const basedir = resolve(dirname(fileURLToPath(import.meta.url)), "..");
+const version = process.env.PKG_VERSION || readPkgInfoVersion(join(basedir, "package.json"));
+
+execSync(`cargo set-version ${version}`, { cwd: "server" });
