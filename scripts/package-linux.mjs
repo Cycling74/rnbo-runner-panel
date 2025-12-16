1f32095436d39b5db6267b79c4cb29345eba9b95
Alex Norman <xnor@cycling74.com>
2025-12-16 13:09:42 -0800

    update debian config, adding architecture, moving files

--- scripts/package-linux.mjs
+++ scripts/package-linux.mjs
@@ -7,6 +7,7 @@ import { readPkgInfoVersion } from "./utils.mjs";
 const { readFileSync, writeFileSync, rmSync, copySync } = fs;
 
 const debian = process.argv.includes("--debian");
+const arch = process.argv.at(-3);
 const outdir = process.argv.at(-2);
 const target = process.argv.at(-1);
 
@@ -17,7 +18,7 @@ const name = process.env.PKG_NAME || "rnbo-runner-panel";
 // cleanup if we have an existing export
 rmSync(join(outdir, "usr"), { recursive: true, force: true });
 
-copySync(join(basedir, "out"), join(outdir, "usr", "share", name, "www"), { overwrite: true } );
+copySync(join(basedir, "client/out"), join(outdir, "usr", "share", name, "www"), { overwrite: true } );
 copySync(join(serverdir, "templates"), join(outdir, "usr", "share", name, "templates"), { overwrite: true } );
 copySync(join(serverdir, "Rocket.toml"), join(outdir, "usr", "share", name, "Rocket.toml"), { overwrite: true } );
 copySync(join(serverdir, "target", target, "release", name), join(outdir, "usr", "bin", name), { overwrite: true } );
@@ -25,11 +26,17 @@ copySync(join(serverdir, "target", target, "release", name), join(outdir, "usr",
 // do debian specific packaging
 if (debian) {
 	const version = process.env.PKG_VERSION || readPkgInfoVersion(join(basedir, "package.json"));
-	// add the version into the control file
-	const control = readFileSync(join(outdir, "DEBIAN", "control.in"), "utf8").replace(/[\s\n]*$/, "") + `\nVersion: ${version}\n`;
-	writeFileSync(join(outdir, "DEBIAN", "control"), control);
 
-	const deb = `${name}_${version}.deb`;
+	// add the version and architecture into the control file
+	const control = readFileSync(join(basedir, "config/debian/DEBIAN", "control.in"), "utf8").replace(/[\s\n]*$/, "") +
+		`\nVersion: ${version}` +
+		`\nArchitecture: ${arch}` +
+		"\n"
+	;
+	writeFileSync(join(outdir, "DEBIAN", "control"), control);
+	rmSync(join(outdir, "DEBIAN", "control.in"));
+
+	const deb = `${name}_${version}_${arch}.deb`;
 	execSync(`dpkg-deb --build . ../${deb}`, { cwd: outdir });
 	console.log(`created ${deb}`);
 }
