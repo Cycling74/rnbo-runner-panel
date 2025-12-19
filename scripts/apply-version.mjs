ae06f1eb8129daec06db7ffc0b57c233f87de0c0
Florian Demmer <florian@cycling74.com>
2025-12-19 16:00:49 +0000

    fixed type-o in the applyversion script

--- scripts/apply-version.mjs
+++ scripts/apply-version.mjs
@@ -6,4 +6,4 @@ import { fileURLToPath } from "url";
 const basedir = resolve(dirname(fileURLToPath(import.meta.url)), "..");
 const version = process.env.PKG_VERSION || readPkgInfoVersion(join(basedir, "server", "package.json"));
 
-execSync(`cargo set-version ${version}`, { cwd: join(basedir, "server", "package.json") });
+execSync(`cargo set-version ${version}`, { cwd: join(basedir, "server") });
